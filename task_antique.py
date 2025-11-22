7607d6b0dcb31034a427ef62210d564cb07ba815
Your Name <you@example.com>
2025-11-21 17:34:37 -0700

    fixed antique task2

--- task_antique.py
+++ task_antique.py
@@ -16,41 +16,6 @@ testset_path = 'data\\dataset_detection\\gpt-4o-2024-08-06_antique_test_1_groupe
 llm_enhanced_trainset_path = 'data\\features\\llm_enhanced_features\\ANTIQUE_train_Qwen3-8B.json'
 llm_enhanced_testset_path = 'data\\features\\llm_enhanced_features\\ANTIQUE_test_Qwen3-8B.json'
 
-# X structure:
-# x1 = ranking vector
-# x2 = llm_rank vector (llm_enhanced_feature.Ranking)
-# x3 = llm Response1 Score (llm_enhanced_feature.Response1 Score)
-# x4 = llm Response2 Score (llm_enhanced_feature.Response1 Score)
-# x5 = llm Response3 Score (llm_enhanced_feature.Response1 Score)
-def task2_features(df):
-    ret = []
-    for k, v in df.iterrows():
-        llm_enhanced_feature = v['llm_enhanced_feature']
-        print(llm_enhanced_feature)
-        if llm_enhanced_feature is dict:
-            ret.append(
-                v['ranking'] +
-                llm_enhanced_feature['Ranking'] +
-                [
-                    llm_enhanced_feature['Response1 Score'], 
-                    llm_enhanced_feature['Response2 Score'],
-                    llm_enhanced_feature['Response3 Score']
-                ]
-            )
-        else:
-            ret.append( ### TODO: Placeholder; fix features if they are missing
-                v['ranking'] +
-                [-1, -1, -1] +
-                [
-                    -1, 
-                    -1,
-                    -1
-                ]
-            )
-
-    return np.array(ret)
-
-
 def main(task_num=5):
     # use Logistic Regression
     # xi f = {f1, f2, f3}, and f is value of possible permutation
@@ -86,44 +51,111 @@ def main(task_num=5):
 
 # Task 2
 
-    llm_feature_score_map = {}
+    llm_feature_score_map = {} # k = query: str, v = docs: dict (k = doc: str, v: pair (sum, count))
 
     # populate scores for llm_features
-
     df_llm_trainset = pd.read_json(llm_enhanced_trainset_path)
-    for k, v in df_llm_trainset.iterrows():
-        sample_query = v['query']
-        sample_llm_enhanced_feature = v['llm_enhanced_feature']
-        if llm_feature_score_map[sample_query] is dict:
-            # TODO: continue working from here
-            pass
-        else:
+    for k, sample in df_llm_trainset.iterrows():
+        sample_query = sample['query']
+        sample_llm_enhanced_feature = sample['llm_enhanced_feature']
+        if sample_query not in llm_feature_score_map:
             llm_feature_score_map[sample_query] = {}
 
-    """df_llm_train = pd.concat([df_train, pd.read_json(llm_enhanced_trainset_path)], ignore_index=True)
-    llm_X_train = task2_features(df_llm_train)
-    llm_y_train = encoder.transform(df_llm_train['label'])
+        if sample_llm_enhanced_feature is None:
+            continue
 
-    df_llm_test = pd.concat([df_test, pd.read_json(llm_enhanced_testset_path)], ignore_index=True)
-    llm_X_test = task2_features(df_llm_test)
-    llm_y_test = encoder.transform(df_llm_test['label'])
+        docs_scores_map = llm_feature_score_map[sample_query]
+        for i in range(3):
+            cur_doc_score = sample_llm_enhanced_feature['Response' + str(i + 1) + ' Score']
+            cur_doc = sample['docs'][i]
+            if cur_doc in docs_scores_map:
+                docs_scores_map[cur_doc] = (docs_scores_map[cur_doc][0] + cur_doc_score, docs_scores_map[cur_doc][1] + 1)
+            else:
+                docs_scores_map[cur_doc] = (cur_doc_score, 1)
+
+    for k, sample in pd.read_json(llm_enhanced_testset_path).iterrows():
+        sample_query = sample['query']
+        sample_llm_enhanced_feature = sample['llm_enhanced_feature']
+        if sample_query not in llm_feature_score_map:
+            llm_feature_score_map[sample_query] = {}
+
+        if sample_llm_enhanced_feature is None:
+            continue
+
+        docs_scores_map = llm_feature_score_map[sample_query]
+        for i in range(3):
+            cur_doc_score = sample_llm_enhanced_feature['Response' + str(i + 1) + ' Score']
+            cur_doc = sample['docs'][i]
+            if cur_doc in docs_scores_map:
+                docs_scores_map[cur_doc] = (docs_scores_map[cur_doc][0] + cur_doc_score, docs_scores_map[cur_doc][1] + 1)
+            else:
+                docs_scores_map[cur_doc] = (cur_doc_score, 1)
+
+    missing_doc_count = 0
+
+    # apply matching llm_features to corresponding samples in train set
+    X_train_task2 = []
+    for k, sample in df_train.iterrows():
+        sample_query = sample['query']
+        if sample_query not in llm_feature_score_map:
+            raise Exception("Query not in llm_feature_score_map")
+
+        sample_docs = sample['docs']
+        if sample_docs is None:
+            raise Exception("Sample docs is None")
+        
+        llm_ranked_scores = []
+
+        for i in range(3):
+            cur_doc = sample_docs[i]
+            cur_doc_score = -1 # signifies that the data set contains a response that has zero data for its llm generated judgement
+            if cur_doc in llm_feature_score_map[sample_query]:
+                cur_doc_score = llm_feature_score_map[sample_query][cur_doc][0] / llm_feature_score_map[sample_query][cur_doc][1]
+            else:
+                missing_doc_count += 1
+
+            llm_ranked_scores.append(cur_doc_score)
+
+        X_train_task2.append(sample['ranking'] + llm_ranked_scores)
+    y_train_task2 = encoder.transform(df_train['label'])
+
+    X_test_task2 = []
+    for k, sample in df_test.iterrows():
+        sample_query = sample['query']
+        if sample_query not in llm_feature_score_map:
+            raise Exception("Query not in llm_feature_score_map")
+
+        sample_docs = sample['docs']
+        if sample_docs is None:
+            raise Exception("Sample docs is None")
+        
+        llm_ranked_scores = []
+
+        for i in range(3):
+            cur_doc = sample_docs[i]
+            cur_doc_score = -1 # signifies that the data set contains a response that has zero data for its llm generated judgement
+            if cur_doc in llm_feature_score_map[sample_query]:
+                cur_doc_score = llm_feature_score_map[sample_query][cur_doc][0] / llm_feature_score_map[sample_query][cur_doc][1]
+            else:
+                missing_doc_count += 1
+
+            llm_ranked_scores.append(cur_doc_score)
+
+        X_test_task2.append(sample['ranking'] + llm_ranked_scores)
+    y_test_task2 = encoder.transform(df_test['label']) 
 
     clf = LogisticRegression()
-    clf.fit(llm_X_train, llm_y_train)
+    clf.fit(X_train_task2, y_train_task2)
 
-    y_pred = clf.predict(llm_X_test)
-    print("Logistic Regression Accuracy:", accuracy_score(llm_y_test, y_pred))
+    y_pred = clf.predict(X_test_task2)
+    print("Logistic Regression Accuracy:", accuracy_score(y_test_task2, y_pred))
 
-    # use RandomForest
     clf = RandomForestClassifier(n_estimators=100, random_state=42)
-    clf.fit(llm_X_train, llm_y_train)
+    clf.fit(X_train_task2, y_train_task2)
 
-    y_pred = clf.predict(llm_X_test)
-    print("Random Forest Accuracy:", accuracy_score(llm_y_test, y_pred))"""
-
-    # Logistic Regression Accuracy: 0.6670811666289849
-    # Random Forest Accuracy: 0.6670811666289849
-    # TODO: Fix overlapping
+    y_pred = clf.predict(X_test_task2)
+    print("Random Forest Classification Accuracy:", accuracy_score(y_test_task2, y_pred))
+    print(f"Missing doc count: {missing_doc_count}")
 
 if __name__ == "__main__":
     main()
